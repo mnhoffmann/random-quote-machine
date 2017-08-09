7ad8437fd5cabfa67920cdd0f2af42583aaf8cd2
Mariana Hoffmann <marinhoffmann@gmail.com>
2017-08-08 22:23:55 -0300

    Changed some lines of the code in order to make things work locally.

--- app.js
+++ app.js
@@ -1,33 +1,1 @@
-$(document).ready(function() {
-	$.ajaxSetup({
-		cache: false //needed in order to update quotes
-	});
-
-	function getNewQuote() {
-		$.getJSON("//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(data) {
-			if(data[0].content.length > 120) {
-				getNewQuote();
-			}
-			else{
-				$("#quoteText").html(data[0].content);
-				var quoteAuthor = " —" + data[0].title; 
-				$("#quoteAuthor").html(quoteAuthor);
-			}
-		});
-	}
-
-	getNewQuote();//get a quote on first load
-	
-	$("#newQuoteButton").click(function() {
-		getNewQuote();
-	});
-
-	$("#tweetButton").on('click', function(event) {
-		event.preventDefault();	
-		window.open("https://twitter.com/intent/tweet?text=" + "\"" + $("#quoteText").text() + "\""+ $("#quoteAuthor").text());
-	});
-  
-  $("#mari").on('click', function(event) {
-    window.open("https://twitter.com/mnhoffmann");
-  });
-});
+$(document).ready(function() {	$.ajaxSetup({		cache: false //needed in order to update quotes	});	function getNewQuote() {		$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(data) {			if(data[0].content.length > 120) {				getNewQuote();			}			else{				$("#quoteText").html(data[0].content);				var quoteAuthor = " —" + data[0].title; 				$("#quoteAuthor").html(quoteAuthor);			}		});	}	getNewQuote();//get a quote on first load		$("#newQuoteButton").click(function() {		getNewQuote();	});	$("#tweetButton").on('click', function(event) {		event.preventDefault();			window.open("https://twitter.com/intent/tweet?text=" + "\"" + $("#quoteText").text() + "\""+ $("#quoteAuthor").text());	});    $("#mari").on('click', function(event) {    window.open("https://twitter.com/mnhoffmann");  });});
